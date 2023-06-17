14ff80ea6cc9094389f2925685725eca63fd35ad
Junghwan Lim <balance139@gmail.com>
2023-06-18 08:33:23 +0900

    23.06.18

--- src/main/java/com/example/spring_order/item/ItemController.java
+++ src/main/java/com/example/spring_order/item/ItemController.java
@@ -4,6 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.sql.SQLException;
+import java.util.List;
 
 @Controller
 public class ItemController {
@@ -16,6 +21,38 @@ public class ItemController {
         model.addAttribute("form",new form());
         return "Items/createItemForm";
     }
+
+    @PostMapping("items/new")
+    public String member_new_post(form item)
+    {
+        itemService.item_save(item);
+        return "redirect:/";
+
+    }
+
+    @GetMapping("items")
+    public String itemlist(Model model) throws SQLException {
+
+        List<Item> items = itemService.item_findall();
+        model.addAttribute("items",items);
+        return "items/itemList";
+
+    }
+
+
+
+    @GetMapping("items/{id}/edit")
+    public String item_update(form item, Model model){
+        model.addAttribute("form",item);
+        return "Items/updateItemForm";
+    }
+
+
+//    @PostMapping()
+//    public String itemUpdate(form item)  throws Exception {
+//        itemService.update(item);
+//        return "redirect:/items/itemList";
+
 
 
 }
--- src/main/java/com/example/spring_order/item/ItemService.java
+++ src/main/java/com/example/spring_order/item/ItemService.java
@@ -12,6 +12,7 @@ public class ItemService {
 
     @Autowired ItemRepository itemRepository;
 
+    // create
     public void item_save(form item){
 
         Item item1 = Item.builder()
@@ -23,13 +24,33 @@ public class ItemService {
 
     }
 
+    // read_all
     public List<Item> item_findall(){
         return itemRepository.findAll();
     }
 
+    // read_one
     public Item item_one(Long id){
         return itemRepository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
+
+    // update
+    public void update(form item) throws Exception{
+        Item item1 = itemRepository.findById(item.getId()).orElseThrow(EntityNotFoundException::new);
+
+        if(item1 == null){
+            throw new Exception();
+        }else{
+
+           item1.setName(item.getName());
+           item1.setPrice(item.getPrice());
+           item1.setStockQuantity(item.getStockQuantity());
+           itemRepository.save(item1);
+
+        }
+
+    }
+
 
 
 }
